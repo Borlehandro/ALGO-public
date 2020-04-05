c0aabb1e79d609d737d97719f6a746b1e5dbc171
Borlehandro <borlehandro@gmail.com>
2020-04-05 21:28:27 +0700

    Change text in the QR codes and remake scanning.

--- app/src/main/java/com/alex_borzikov/newhorizonstourism/activities/CodeScanActivity.java
+++ app/src/main/java/com/alex_borzikov/newhorizonstourism/activities/CodeScanActivity.java
@@ -1,6 +1,8 @@
 package com.alex_borzikov.newhorizonstourism.activities;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,8 @@ import com.google.zxing.Result;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class CodeScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private static final String TAG = "Borlehandro";
 
     ZXingScannerView scannerView;
 
@@ -24,7 +28,8 @@ public class CodeScanActivity extends AppCompatActivity implements ZXingScannerV
 
     @Override
     public void handleResult(Result result) {
-        MainActivity.pointCode = result.getText();
+        MainActivity.pointCode = result.getText().substring(result.getText().indexOf("=")+1);
+        Log.d(TAG, "handleResult: " + MainActivity.pointCode);
         onBackPressed();
     }
 
